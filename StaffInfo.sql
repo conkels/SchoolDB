1373fefc8f51bacbe156e35de91f9ede89edeb53
conkel <conorkelly10@aol.com>
2022-01-28 12:04:13 +0000

    adding staff info

---
+++ StaffInfo.sql
@@ -0,0 +1,8 @@
+USE SchoolDB;
+
+INSERT INTO staff (fullName, occupation, email, emergencyContactPhone, emergencyContactName, emergencyContactRelation)
+VALUES ('Michael Smith', 'History Teacher', 'ms_hist@school.com', 07654321234, 'Susan Smith', 'Wife'),
+('Michelle Hart', 'Head of Maths', 'mh_hom@school.com', '07386291042', 'Sam Hart', 'Son'),
+('Harry Hannan', 'English Teacher', 'hh_eng@school.com', '07295718834', 'Matt Hannan', 'Brother'),
+('Hope Kiston', 'Head of Science', 'hk_hos@school.com', '07333578901', 'Mary Miller', 'Aunty'),
+('Kane Manion', 'Head Teacher', 'km_ht@school.com', '07376769001', 'Katherine Manion', 'Wife');
